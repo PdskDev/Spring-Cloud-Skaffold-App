e1c722e898078ac653527b1b36a93fe3f50a0934
PdskDev <prestadesk@gmail.com>
2023-06-06 21:29:26 +0200

    Initial commit

---
+++ src/main/java/com/nadetdev/cloudapp/CloudAppApplication.java
@@ -0,0 +1,14 @@
+package com.nadetdev.cloudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CloudAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nadetdev/cloudapp/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.nadetdev.cloudapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(
+            @RequestParam(defaultValue = "NadetDev", name = "dev", required = false) String dev,
+            Model model){
+        model.addAttribute("dev", dev);
+        return "index";
+    }
+}
